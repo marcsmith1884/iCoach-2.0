84f026fb0cc7b17dad6830a394b7e0c2ebc5b545
Ubuntu <ubuntu@ip-172-31-91-166.ec2.internal>
2022-07-27 22:07:14 +0000

    bootstrap installed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,8 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+require("stylesheets/application.scss")
