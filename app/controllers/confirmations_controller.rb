2e3f0c1ad72a3d1fc7ebaeca8825d883f437e809
Ubuntu <ubuntu@ip-172-31-91-166.ec2.internal>
2022-07-30 15:19:33 +0000

    the users index and the user show pages and the trackable gem

---
+++ app/controllers/confirmations_controller.rb
@@ -0,0 +1,7 @@
+class ConfirmationsController < Devise::ConfirmationsController
+    private
+    def after_confirmation_path_for(resource_name, resource)
+        sign_in(resource) # Just in case you would like to sign in the user
+        root_path
+    end
+end     
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+    
+    def index
+        @users = User.all.order(created_at: :asc)
+    end
+    
+    def show
+        @user = User.find(params[:id])
+    end
+    
+end
