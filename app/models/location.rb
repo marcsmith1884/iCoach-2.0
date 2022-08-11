ec5b9b71666b6ecba4b569411fd7c36d2e27f8c0
Your Name <you@example.com>
2022-08-11 23:04:08 +0000

    Validate Locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,12 @@
 class Location < ApplicationRecord
+    
+    validates :name, presence: true, uniqueness: true
+    validates :name, uniqueness: { case_sensitive: false }
+    validates :duration, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 180}
+    
+    
+    def to_s
+     name
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
 User.create(email: "hurling@example.com", password: "hurling@example.com", password_confirmation: "hurling@example.com")
 User.create(email: "player1@example.com", password: "player1@example.com", password_confirmation: "player1@example.com")
 User.update_all confirmed_at: DateTime.now
+
+
+
+Training.create(name: "Hurling")
+Training.create(name: "Gaelic")
+Training.create(name: "Gym")
+
+Location.create(name: "Balheary", duration: 45)
+Location.create(name: "Lawless Park", duration: 55)
